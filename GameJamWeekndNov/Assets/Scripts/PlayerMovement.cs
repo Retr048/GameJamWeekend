d8b37c32df20d554426f8583e79ae4d63b6f0a98
Vladyjokey <vladislav.vvd05@gmail.com>
2022-11-19 21:52:29 +0200

    movement and jump fixes

--- GameJamWeekndNov/Assets/Scripts/PlayerMovement.cs
+++ GameJamWeekndNov/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
    [SerializeField] float Speed = 2f; 
    Vector3 movement;
+   Vector3 lookRotVector;
    bool jumped = false;
    float jumpTimer;
 
@@ -26,21 +27,30 @@ public class PlayerMovement : MonoBehaviour
         float x =  Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        if(!jumped)
-            movement = new Vector3(x, movement.y, z);  
-        //removing the .normalized would make the speed double time faster. It also adds a little delay when stopping.        
+        movement = new Vector3(x, movement.y, z);  
     }    
 
     void Movement()
     {
-        transform.Translate(movement.normalized * Speed * Time.deltaTime, Space.World);
+        //removing the .normalized would make the speed double time faster. It also adds a little delay when stopping.  
+        if(jumped){
+            movement.x = Mathf.Clamp(movement.x, -0.25f, 0.25f);
+            movement.z = Mathf.Clamp(movement.z, -0.25f, 0.25f);
+            transform.Translate(Vector3.ClampMagnitude(movement, 1.25f) * Speed * Time.deltaTime, Space.World);
+        }
+        else{
+            transform.Translate(Vector3.ClampMagnitude(movement, 1f) * Speed * Time.deltaTime, Space.World);
+        }
     }
 
     void LookRotation()
     {
-        if(movement != Vector3.zero && !jumped)
+        lookRotVector = movement;
+        lookRotVector.y = 0;
+
+        if(lookRotVector != Vector3.zero)
         {
-            Quaternion rotate = Quaternion.LookRotation(new Vector3(movement.x ,0 ,movement.z), Vector3.up);
+            Quaternion rotate = Quaternion.LookRotation(lookRotVector, Vector3.up);
             transform.rotation = rotate;
         }
     }
@@ -49,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.Space) && jumpTimer + 1f<= Time.time){
             jumped = true;
             jumpTimer = Time.time;
-            movement.y = 5;
+            movement.y = 1f;
         }else if(jumpTimer + 1f <= Time.time){
             movement.y = 0;
             jumped = false;
