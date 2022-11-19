dbfe320b74ad47303da3790ef8cf7a1bbd8b9199
Retr0 <mihaildrumev@gmail.com>
2022-11-19 12:58:54 +0200

    LookRotation

--- GameJamWeekndNov/Assets/Scripts/PlayerController.cs
+++ GameJamWeekndNov/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         move.Move();
     }
--- GameJamWeekndNov/Assets/Scripts/PlayerMovement.cs
+++ GameJamWeekndNov/Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
    [SerializeField] float Speed = 2f; 
-   Transform player;
+   [SerializeField] Transform player;
+   Vector3 movement;
 
      void Start()
     {
-        player = GetComponent<Transform>();
+        
     }
+
     public void Move()
+    {
+        InputTake();
+        LookRotation();
+        Movement();
+    }
+
+
+    public void InputTake()
     {
         float x =  Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(x, 0, z).normalized * Speed * Time.deltaTime;  
-        //removing the .normalized would make the speed double time faster. It also adds a little delay when stopping.
-
-        player.Translate(movement);
+        movement = new Vector3(x, 0, z);  
+        //removing the .normalized would make the speed double time faster. It also adds a little delay when stopping.        
     }    
+
+    void Movement()
+    {
+        this.transform.Translate(movement.normalized * (Speed * Time.deltaTime));
+    }
+
+    void LookRotation()
+    {
+        if(movement != Vector3.zero)
+        {
+            Quaternion rotate = Quaternion.LookRotation(movement, Vector3.up);
+            player.rotation = rotate;
+        }
+    }
+
 }
+
+
+
